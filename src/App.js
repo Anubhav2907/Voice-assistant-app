a78403be732ab21bf3ed8419b45c041e3d9231d5
Anubhav2907 <u19ec135@eced.svnit.ac.in>
2021-06-13 04:37:09 +0530

    automatic scroll left

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from "./components/NewsCards/NewsCards";
-
+import useStyles from "./styles";
 const alanKey =
   "8b767f986c749b0462768ef6bf5331df2e956eca572e1d8b807a3e2338fdd0dc/stage";
 //38b4c8d9046444d0805453dd00a2828d
 const App = () => {
+  const classes = useStyles();
   const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setactiveArticle] = useState(-1);
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
       onCommand: ({ command, articles }) => {
         if (command === "newHeadlines") {
           setNewsArticles(articles);
+          setactiveArticle(-1);
+        } else if (command === "highlight") {
+          setactiveArticle((prevArticle) => prevArticle + 1);
         }
       },
     });
   }, []);
   return (
     <div>
-      <NewsCards articles={newsArticles}></NewsCards>
+      <div className={classes.logoContainer}>
+        <img
+          src="https://voicebot.ai/wp-content/uploads/2019/10/alan.jpg"
+          alt="alan-images"
+          className={classes.alanLogo}
+        ></img>
+      </div>
+      <NewsCards
+        articles={newsArticles}
+        activeArticle={activeArticle}
+      ></NewsCards>
     </div>
   );
 };
