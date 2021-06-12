d183e0f92b7c9e84d2964be94345d269bad6d065
Anubhav2907 <u19ec135@eced.svnit.ac.in>
2021-06-13 05:02:13 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from "./components/NewsCards/NewsCards";
 import useStyles from "./styles";
+import wordsToNumbers from "words-to-numbers";
 const alanKey =
   "8b767f986c749b0462768ef6bf5331df2e956eca572e1d8b807a3e2338fdd0dc/stage";
 //38b4c8d9046444d0805453dd00a2828d
@@ -13,12 +14,26 @@ const App = () => {
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command, articles }) => {
+      onCommand: ({ command, articles, number }) => {
         if (command === "newHeadlines") {
           setNewsArticles(articles);
           setactiveArticle(-1);
         } else if (command === "highlight") {
           setactiveArticle((prevArticle) => prevArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+          if (parsedNumber > 20) {
+            alanBtn().playText("Please try that again");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening...");
+          } else {
+            alanBtn().playText("Try Again...");
+          }
         }
       },
     });
